4be50079f7c8261ba4dc5bfa571b89ca1742652e
GuillermoCruz27 <joadex27@gmail.com>
2024-10-27 18:25:36 -0300

    Inicio del Trabajo Practico Nro 6

---
+++ README.md
@@ -0,0 +1,5 @@
+# Linked List Double
+Desarrollo de ejercicios de listas doblemente enlazadas
+
+## Asignatura
+Estructura de Datos
---
+++ TP-Nro6/Exercise-12/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Con la ayuda de una TDA Bicola escribir un programa que permita determinar si una oración es o no palíndroma.
+ * Tenga presente que para que esto se cumpla no deberian considerarse los espacios y no considerar mayúsculas.
+ */
+
+int main() {
+    return 0;
+}
